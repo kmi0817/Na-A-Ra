52b068530771ca37bc2c0531c3c69f78c10c6e1f
kmi0817 <kmi0817@naver.com>
2022-01-04 14:00:50 +0900

    necessary hospital info print

--- server.js
+++ server.js
@@ -23,7 +23,8 @@ app.get("/", (req, res) => {
 app.get("/openapi", (req, res) => {
     // OpenAPI
     var url = 'http://apis.data.go.kr/B551182/hospInfoService1/getHospBasisList1';
-    var queryParams = '?' + encodeURIComponent('serviceKey') + '=JtIV8PCzx8%2BLWnp%2F07kxb%2FL4%2Fglq9W6WGZN2AQwOBG%2B9fIRQEA%2F12X%2F2ONTYaEFLDPxdBzqz1CWa6%2FRDwcMxRA%3D%3D'; /* Service Key*/
+    const myServiceKey = "";
+    var queryParams = '?' + encodeURIComponent('serviceKey') + '='  + myServiceKey; /* Service Key*/
 
     request({
         // 응답표준은 XML이며, JSON을 요청할 경우 “&_type=json”을 추가하여 요청합니다.
@@ -31,9 +32,23 @@ app.get("/openapi", (req, res) => {
         method: "GET"
     }, (err, response, body) => {
         console.log(`Status: ${response.statusCode}`);
-        console.log(`Headers: ${JSON.stringify(response.headers)}`);
-        hospitals = JSON.parse(body);
-        res.send(hospitals);
+        parsed_body = JSON.parse(body);
+        hospitals_list = parsed_body["response"]["body"]["items"]["item"];
+
+        res_list = []
+        hospitals_list.forEach((hospital, index) => {
+            res_list.push({
+                "인덱스": index,
+                "병원명": hospital["yadmNm"],
+                "주소": hospital["addr"],
+                "종별코드명": hospital["clCdNm"],
+                "전화번호": hospital["telno"],
+                "의사총수": hospital["drTotCnt"],
+                "좌표": [hospital["XPos"], hospital["YPos"]]
+            })
+        });
+
+        res.send(JSON.stringify(res_list, null, 4));
     }
     );
 })

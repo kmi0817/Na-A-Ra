c820618be88969e9b11ed30016416c3403d925e5
kmi0817 <kmi0817@naver.com>
2022-01-05 14:21:22 +0900

    use select option (병원명, 읍/면/동 선택) & 검색

--- server.js
+++ server.js
@@ -18,16 +18,24 @@ app.engine("html", require("ejs").renderFile);
 // routes
 app.get("/", (req, res) => {
     // req.query 객체에 input 내용 있음
-    var emdong = req.query.emdong;
-    console.log(`emdong: ${emdong}`);
-    
-    if (typeof emdong != "undefined") {
+    var search_type = req.query.search_type;
+    console.log(search_type);
+    if (typeof search_type == "undefined") {
+        res.render("index.html");
+    } else {
         var url = 'http://apis.data.go.kr/B551182/hospInfoService1/getHospBasisList1';
         const myServiceKey = "JtIV8PCzx8%2BLWnp%2F07kxb%2FL4%2Fglq9W6WGZN2AQwOBG%2B9fIRQEA%2F12X%2F2ONTYaEFLDPxdBzqz1CWa6%2FRDwcMxRA%3D%3D";
-        var queryParams = '?' + encodeURIComponent('serviceKey') + '='  + myServiceKey; /* Service Key*/
-        queryParams += '&' + encodeURIComponent('emdongNm') + '=' + encodeURIComponent(emdong);
+        var queryParams = '?' + encodeURIComponent('serviceKey') + '=' + myServiceKey; /* Service Key*/
+        queryParams += '&' + encodeURIComponent('numOfRows') + '=' + encodeURIComponent('5');
+        
+        var seacrh_input = req.query.seacrh_input;
 
-        res_list = []
+        if (search_type == "yadmNm") {
+            queryParams += '&' + encodeURIComponent('yadmNm') + '=' + encodeURIComponent(seacrh_input);
+        } else if (search_type == "emdongNm") {
+            queryParams += '&' + encodeURIComponent('emdongNm') + '=' + encodeURIComponent(seacrh_input);
+        }
+
         request({
             // 응답표준은 XML이며, JSON을 요청할 경우 “&_type=json”을 추가하여 요청합니다.
             url: url + queryParams + "&_type=json",
@@ -39,7 +47,7 @@ app.get("/", (req, res) => {
             res_list = []
             hospitals_list.forEach((hospital, index) => {
                 res_list.push({
-                    "인덱스": index,
+                    "인덱스": index + 1,
                     "병원명": hospital["yadmNm"],
                     "주소": hospital["addr"],
                     "종별코드명": hospital["clCdNm"],
@@ -48,7 +56,6 @@ app.get("/", (req, res) => {
                     "좌표": [hospital["XPos"], hospital["YPos"]]
                 })
             });
-            console.log(res_list);
             res.render("index.ejs", {"data": res_list}, (err, html) => {
                 if (err) {
                     console.error(err);
@@ -56,11 +63,8 @@ app.get("/", (req, res) => {
                     res.end(html);
                 }
             })
-            // res.send(JSON.stringify(res_list, null, 4));
         }
         );
-    } else {
-        res.render("index.html");
     }
 });
 

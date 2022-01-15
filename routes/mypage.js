77eadde06f0296eea0755d74d0238ce70bbe3837
kmi0817 <kmi0817@naver.com>
2022-01-15 18:30:34 +0900

    add put("/edit/:id") & write-edit use same func

--- routes/mypage.js
+++ routes/mypage.js
@@ -26,24 +26,39 @@ router.get("/write", (req, res) => {
     res.render("mypage/write", {results: new Article() });
 });
 
-router.post("/process-write", async (req, res) => {
-    let result = new Article({
-        title: req.body.title,
-        description: req.body.description,
-        markdown: req.body.markdown
-    });
-    try {
-        result = await result.save();
-        console.log(result.id);
-        res.redirect(`/mypage/show/${result.slug}`);
-    } catch (err) {
-        res.render('mypage/write', { results: result});
-    }
-});
+router.get("/edit/:id", async (req, res) => {
+    const result = await Article.findById(req.params.id);
+    res.render("mypage/edit", { results: result });
+})
+
+router.post("/process-write", async (req, res, next) => {
+    req.article = new Article();
+    next();
+}, saveArticleAndRedirect('write'));
+
+router.put("/edit/:id", async (req, res, next) => {
+    req.article = await Article.findById(req.params.id);
+    next();
+}, saveArticleAndRedirect('edit'));
 
 router.delete("/del/:id", async (req, res) => {
     await Article.findByIdAndDelete(req.params.id);
     res.redirect("/mypage");
 });
 
+function saveArticleAndRedirect(path) {
+    return async (req, res) => {
+        let result = req.article;
+        result.title = req.body.title;
+        result.description = req.body.description;
+        result.markdown = req.body.markdown;
+        try {
+            result = await result.save();
+            console.log(result.id);
+            res.redirect(`/mypage/show/${result.slug}`);
+        } catch (err) {
+            res.render(`mypage/${path}`, { results: result});
+        }
+    }
+}
 module.exports = router

7499f89cac8485087b03d6a663d2146fd4e1e885
hyj3463#naver.com <hyj3463@naver.com>
2022-01-26 19:39:11 +0900

    first commit

---
+++ routes/admin.js
@@ -0,0 +1,118 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+const request = require("request-promise-native");
+const Hospitals = require("../models/hospitals");
+const router = express.Router();
+
+mongoose.connect("mongodb://localhost/app", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+const connection = mongoose.connection;
+connection.once("open", () => {
+    console.log("MongoDB OK");
+});
+
+router.get("/", (req, res) => {
+    res.render("admin/index");
+    //res.sendFile(path.join(__dirname, '../design/build/index.html'))
+});
+
+router.get("/json", async (req, res) => {
+    const results = await Hospitals.find().limit(50).sort({name: 1});
+    res.send(results);
+})
+
+router.get("/hospitals", async (req, res) => {
+    const results = await Hospitals.find().limit(50).sort({name: 1});
+    res.render("admin/hospitals", { results: results });
+});
+
+router.get("/hospitals/:id", async(req, res) => {
+    const results = await Hospitals.findOne({ _id: req.params.id });
+    if (results == null)
+        res.redirect("/admin");
+
+    res.render("admin/hospital", { results: results });
+});
+
+router.delete("/hospitals/:id", async(req, res) => {
+    await Hospitals.findByIdAndDelete({ _id: req.params.id });
+    res.redirect("/admin/hospitals");
+});
+
+router.post("/hospitals/:zipCd", async (req, res) => {
+    const _url = 'http://apis.data.go.kr/B551182/hospInfoService1/getHospBasisList1';
+    const myServiceKey = "JtIV8PCzx8%2BLWnp%2F07kxb%2FL4%2Fglq9W6WGZN2AQwOBG%2B9fIRQEA%2F12X%2F2ONTYaEFLDPxdBzqz1CWa6%2FRDwcMxRA%3D%3D";
+    let queryParams = '?' + encodeURIComponent('serviceKey') + '='  + myServiceKey;
+    queryParams += '&' + encodeURIComponent('pageNo') + '=' + encodeURIComponent('1');
+
+    /* 분류코드 */
+    const zipCd = String(req.params.zipCd);
+    let totalCount;
+    if (zipCd == "2010") { totalCount = 365; await Hospitals.deleteMany({zipCd: zipCd}); } // 종합병원
+    else if (zipCd == "2030") { totalCount = 1397; await Hospitals.deleteMany({zipCd: zipCd}); } // 병원
+    else if (zipCd == "2040") { totalCount = 1463; await Hospitals.deleteMany({zipCd: zipCd}); } // 요양병원
+    else if (zipCd == "2050") { totalCount = 18860; await Hospitals.deleteMany({zipCd: zipCd}); } // 치과
+    else if (zipCd == "2060") { totalCount = 15025; await Hospitals.deleteMany({zipCd: zipCd}); } // 한방
+    else if (zipCd == "2070") { totalCount = 33962; await Hospitals.deleteMany({zipCd: zipCd}); } // 의원
+    else if (zipCd == "2080") { totalCount = 3483; await Hospitals.deleteMany({zipCd: zipCd}); } // 보건기관
+    else if (zipCd == "2090") { totalCount = 16; await Hospitals.deleteMany({zipCd: zipCd}); }// 조산원
+    else { console.log("*** Error - no matching code"); }
+
+    queryParams += '&' + encodeURIComponent('zipCd') + '=' + encodeURIComponent(zipCd);
+    queryParams += '&' + encodeURIComponent('numOfRows') + '=' + encodeURIComponent(totalCount);
+    queryParams += "&_type=json"; // To change XML (default response data type) to JSON, append "&_type=json"
+    const options = {
+        url: _url + queryParams,
+        method: "GET"
+    }
+
+    const response = await request(options); // request to OpenAPI
+
+    console.log(`zipCd= ${zipCd}`);
+    console.log("totalCount= " + JSON.stringify(JSON.parse(response)["response"]["body"]["totalCount"]));
+
+    results = JSON.parse(response)["response"]["body"]["items"]["item"] // extract hospital data only
+
+    results.forEach( async (result) => {
+        let hospital = new Hospitals();
+        hospital.addr = result.addr;
+        hospital.type_code = result.clCdNm;
+        hospital.cmdcGdrCnt = result.cmdcGdrCnt;
+        hospital.cmdcIntnCnt = result.cmdcIntnCnt;
+        hospital.cmdcResdntCnt = result.cmdcResdntCnt;
+        hospital.cmdcSdrCnt = result.cmdcSdrCnt;
+        hospital.detyGdrCnt = result.detyGdrCnt;
+        hospital.detyIntnCnt = result.detyIntnCnt;
+        hospital.detyResdntCnt = result.detyResdntCnt;
+        hospital.detySdrCnt = result.detySdrCnt;
+        hospital.drTotCnt = result.drTotCnt;
+        hospital.estbDd = result.estbDd;
+        hospital.hospUrl = result.hospUrl;
+        hospital.mdeptGdrCnt = result.mdeptGdrCnt;
+        hospital.mdeptIntnCnt = result.mdeptIntnCnt;
+        hospital.mdeptResdntCnt = result.mdeptResdntCnt;
+        hospital.mdeptSdrCnt = result.mdeptSdrCnt;
+        hospital.postNo = result.postNo;
+        hospital.sgguCdNm = result.sgguCdNm;
+        hospital.sidoCdNm = result.sidoCdNm;
+        hospital.telno = result.telno;
+        hospital.coord = [result.XPos, result.YPos];
+        hospital.name = result.yadmNm;
+        hospital.zipCd = zipCd;
+
+        try {
+            hospital = await hospital.save();
+        } catch (error) {
+            console.log("DB 저장 문제: " + error);
+        }
+    });
+    res.redirect("/admin/hospitals");
+});
+
+router
+
+
+module.exports = router
---
+++ server.js
@@ -0,0 +1,168 @@
+const express = require("express");
+const request = require("request");
+const path = require("path");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const adminRouter = require("./routes/admin");
+const Hospitals = require("./models/hospitals");
+const methodOverride = require("method-override");
+const app = express();
+const port = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: false })); // enable to use req.body.(input field)
+
+// views engine (convert ejs code to HTML)
+app.set("views", __dirname + "/views");
+app.set("view engine", "ejs");
+app.use(methodOverride('_method'));
+app.engine("html", require("ejs").renderFile);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// admin routes
+app.use("/admin", adminRouter);
+
+// routes
+app.get("/", async (req, res) => {
+    const symptom_level = req.query.symptom_level;
+    const symptom = req.query.symptom;
+
+    if (typeof symptom_level == "undefined" || typeof symptom == "undefined") {
+        res.render("index", { results: "" });
+    } else {
+        console.log(`증상 정도: ${symptom_level}, 증상: ${symptom}`);
+
+        /*
+            0 - 내과
+            1 - 안과
+            2 - 정형외과
+            3 - 외과
+            4 - 치과
+        */
+        let rules = {
+            0: [ "머리", "울려", "두통", "골", "코", "막힘", "막혀", "콧물", "훌쩍" ],
+            1: [ "눈", "충혈", "시력", "흐릿" ],
+            2: [ "뼈", "골절", "삐었", "금", "절뚝", "뚝", "관절", "무릎", "손목", "발목" ],
+            3: [ "항문", "상처", "꼬매" ],
+            4: [ "치아", "이", "치통", "썩었", "썩음", "썩어" ]
+        }
+        let response = {
+            0: { zipCd: "2070", name: { $regex: "내과" } },
+            1: { zipCd: "2070", name: { $regex: "안과" } },
+            2: { zipCd: "2070", name: { $regex: "정형외과"} },
+            3: { zipCd: "2070", $and : [{name : {$regex : "외과"}}, {name : {$not : {$regex : "정형"}}}] },
+            4: { zipCd: "2050", name: { $regex: "의원" } }
+        }
+        let results;
+        for (rule in rules) {
+            flag = false
+            let words = rules[rule];
+            for (word in words) {
+                if (symptom.includes(words[word])) {
+                    flag = true
+                    break
+                }
+            }
+            if (flag) {
+                results = await Hospitals.find(response[rule]).limit(20);
+            }
+        }
+
+        // Send index.ejs data
+        if (typeof results != "undefined") {
+            //res.render("index", { results: results });
+            res.json(results);
+        } else {
+            res.render("index", { results: 'no' });
+        }
+    }
+});
+
+
+app.get("/openapi", (req, res) => {
+    // OpenAPI
+    var url = 'http://apis.data.go.kr/B551182/hospInfoService1/getHospBasisList1';
+    const myServiceKey = "JtIV8PCzx8%2BLWnp%2F07kxb%2FL4%2Fglq9W6WGZN2AQwOBG%2B9fIRQEA%2F12X%2F2ONTYaEFLDPxdBzqz1CWa6%2FRDwcMxRA%3D%3D";
+    var queryParams = '?' + encodeURIComponent('serviceKey') + '=' + myServiceKey; /* Service Key*/
+    queryParams += '&' + encodeURIComponent('sidoCd') + '=' + encodeURIComponent("110000");
+
+    request({
+        // 응답표준은 XML이며, JSON을 요청할 경우 “&_type=json”을 추가하여 요청합니다.
+        url: url + queryParams + "&_type=json",
+        method: "GET"
+    }, (err, response, body) => {
+        console.log(`Status: ${response.statusCode}`);
+        parsed_body = JSON.parse(body);
+        hospitals_list = parsed_body["response"]["body"]["items"]["item"];
+
+        res_list = []
+        hospitals_list.forEach((hospital, index) => {
+            res_list.push({
+                "인덱스": index,
+                "emdongNm": hospital["emdongNm"],
+                "병원명": hospital["yadmNm"],
+                "주소": hospital["addr"],
+                "종별코드명": hospital["clCdNm"],
+                "전화번호": hospital["telno"],
+                "의사총수": hospital["drTotCnt"],
+                "좌표": [hospital["XPos"], hospital["YPos"]],
+                "시도코드": hospital["sidoCd"],
+                "시도명": hospital["sidoCdNm"]
+            })
+        });
+
+        res.send(JSON.stringify(res_list, null, 4));
+    }
+    );
+});
+
+
+
+
+
+
+
+app.get("/testapi", async (req, res) => {
+    const symptom_level = req.query.symptom_level;
+    const symptom = req.query.symptom;
+
+    if (typeof symptom_level != "undefined" || typeof symptom != "undefined") {
+        console.log(`증상 정도: ${symptom_level}, 증상: ${symptom}`);
+        let rules = {
+            0: [ "머리", "울려", "두통", "골", "코", "막힘", "막혀", "콧물", "훌쩍" ],
+            1: [ "눈", "충혈", "시력", "흐릿" ],
+            2: [ "뼈", "골절", "삐었", "금", "절뚝", "뚝", "관절", "무릎", "손목", "발목" ],
+            3: [ "항문", "상처", "꼬매" ],
+            4: [ "치아", "이", "치통", "썩었", "썩음", "썩어" ]
+        }
+        let response = {
+            0: { zipCd: "2070", name: { $regex: "내과" } },
+            1: { zipCd: "2070", name: { $regex: "안과" } },
+            2: { zipCd: "2070", name: { $regex: "정형외과"} },
+            3: { zipCd: "2070", $and : [{name : {$regex : "외과"}}, {name : {$not : {$regex : "정형"}}}] },
+            4: { zipCd: "2050", name: { $regex: "의원" } }
+        }
+        let results;
+        for (rule in rules) {
+            flag = false
+            let words = rules[rule];
+            for (word in words) {
+                if (symptom.includes(words[word])) {
+                    flag = true
+                    break
+                }
+            }
+            if (flag) {
+                results = await Hospitals.find(response[rule]).limit(20);
+            }
+        }
+
+        res.send(results);
+    }
+});
+
+
+// web server
+const server = app.listen(port, () => {
+    // port: 3000 app success
+    console.log("localhost on port: " + port);
+})

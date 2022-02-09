7cb84c92e1b9761dd2001cab61217851424f5437
hyj3463#naver.com <hyj3463@naver.com>
2022-02-09 09:49:30 +0900

    편의성 버튼  추가

--- design/src/components/MenuBar.js
+++ design/src/components/MenuBar.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Header extends Component {
     render() {
       return (
         <header>
-          <h2 className="menuText">나아라</h2>
-
+          <Link to="/">
+          <h2 className="menuText"><a className="mainPageLink">나아라</a></h2>
+          </Link>
           <nav>
             <ul className="menu">
               <li id="HeaderSignin">로그인</li>

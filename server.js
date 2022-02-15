b12a49cf38de70a3c2f15cf00fb2bb19a34c7593
kmi0817 <kmi0817@naver.com>
2022-02-15 10:01:51 +0900

    code organization

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const request = require("request");
 const adminRouter = require("./routes/admin");
 const commentsRouter = require("./routes/comments");
 const Hospitals = require("./models/hospitals");
@@ -114,7 +113,6 @@ app.get("/", async (req, res) => {
 
         // Send index.ejs data
         if (results.length > 0) {
-            let hospital_ids = [];
             if (req.session.user) {
                 res.render("index_user", { results: results, user_id: req.session.user['id'] });
             } else if (req.session.admin) {
@@ -134,9 +132,8 @@ app.get("/", async (req, res) => {
     }
 });
 
-app.get("/test", (req, res) => {
-    res.render("test");
-})
+app.get("/test", (req, res) => { res.render("test"); });
+
 app.post("/process/:type", async(req, res) => {
     const type = req.params.type;
 
@@ -187,51 +184,10 @@ app.post("/process/:type", async(req, res) => {
             res.send(`<script>alert("일치하는 회원 정보가 없습니다."); history.go(-1);</script>`);
         }
     } else if (type == "logout") {
-        req.session.destroy((error) => {
-            console.log("** logout");
-            res.redirect("/");
-        })
+        req.session.destroy((error) => { res.redirect("/"); });
     }
 
 });
-
-app.get("/openapi", (req, res) => {
-    // OpenAPI
-    var url = 'http://apis.data.go.kr/B551182/hospInfoService1/getHospBasisList1';
-    const myServiceKey = "JtIV8PCzx8%2BLWnp%2F07kxb%2FL4%2Fglq9W6WGZN2AQwOBG%2B9fIRQEA%2F12X%2F2ONTYaEFLDPxdBzqz1CWa6%2FRDwcMxRA%3D%3D";
-    var queryParams = '?' + encodeURIComponent('serviceKey') + '=' + myServiceKey; /* Service Key*/
-    queryParams += '&' + encodeURIComponent('sidoCd') + '=' + encodeURIComponent("110000");
-
-    request({
-        // 응답표준은 XML이며, JSON을 요청할 경우 “&_type=json”을 추가하여 요청합니다.
-        url: url + queryParams + "&_type=json",
-        method: "GET"
-    }, (err, response, body) => {
-        console.log(`Status: ${response.statusCode}`);
-        parsed_body = JSON.parse(body);
-        hospitals_list = parsed_body["response"]["body"]["items"]["item"];
-
-        res_list = []
-        hospitals_list.forEach((hospital, index) => {
-            res_list.push({
-                "인덱스": index,
-                "emdongNm": hospital["emdongNm"],
-                "병원명": hospital["yadmNm"],
-                "주소": hospital["addr"],
-                "종별코드명": hospital["clCdNm"],
-                "전화번호": hospital["telno"],
-                "의사총수": hospital["drTotCnt"],
-                "좌표": [hospital["XPos"], hospital["YPos"]],
-                "시도코드": hospital["sidoCd"],
-                "시도명": hospital["sidoCdNm"]
-            })
-        });
-
-        res.send(JSON.stringify(res_list, null, 4));
-    }
-    );
-});
-
 
 app.use("/admin", adminRouter);
 app.use("/comments", commentsRouter);
@@ -240,7 +196,7 @@ app.use("/comments", commentsRouter);
 server.listen(3000, () => {
     // port: 3000 app success
     console.log("localhost:3000 OK");
-})
+});
 // const server = app.listen(3000, () => {
 //     // port: 3000 app success
 //     console.log("localhost:3000 OK");

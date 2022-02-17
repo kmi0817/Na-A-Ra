23eb1f9982fc89f8c39ad2c45c69cc74ebdb18d2
kmi0817 <kmi0817@naver.com>
2022-02-17 13:46:39 +0900

    list up comments which the member posted

--- routes/mypage.js
+++ routes/mypage.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const Users = require("../models/users");
+const Comments = require("../models/comments");
 const router = express.Router();
 
 router.get("/", async(req, res) => {
     if (req.session.user) {
         const home_results = await Users.findOne({ user_id: req.session.user.id });
-        res.render("mypage", { home_results: home_results });
+        const comments_results = await Comments.find({ writer_id: req.session.user.id }).sort({ _id: -1 });
+        console.log(comments_results)
+        res.render("mypage", { home_results: home_results, comments_results: comments_results });
     } else {
         res.status(404).send("not found");
     }

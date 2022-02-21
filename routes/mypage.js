a5dd4cedd53b64b4668236b61b228da50ee22f08
kmi0817 <kmi0817@naver.com>
2022-02-21 23:59:33 +0900

    change how to load user data (findOne -> findById)

--- routes/mypage.js
+++ routes/mypage.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 router.get("/", async(req, res) => {
     if (req.session.user) {
-        const home_results = await Users.findOne({ user_id: req.session.user.id });
+        const home_results = await Users.findById(req.session.user.id);
         const comments_results = await Comments.find({ writer_id: req.session.user.id }).sort({ _id: -1 });
         res.render("mypage", { home_results: home_results, comments_results: comments_results });
     } else {

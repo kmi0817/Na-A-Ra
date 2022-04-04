d8bdb0f54b8f594c6db088dad55d827221b363c6
kmi0817 <kmi0817@naver.com>
2022-04-04 16:37:43 +0900

    feat: save writings in the database

--- routes/community.js
+++ routes/community.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const Communities = require("../models/communities");
 const request = require("request-promise-native");
 const router = express.Router();
 
@@ -16,6 +17,25 @@ router.get("/clinics/:id", async (req, res) => {
     res.render("community/post")
 });
 
+router.get("/clinics-write", async (req, res) => {
+    res.render("community/write");
+})
+
+router.post("/clinics-post", async (req, res) => {
+    try {
+        let posting = new Communities();
+        posting.title = req.body.inputTitle;
+        posting.description = req.body.inputDescription;
+        posting.community = "clinics";
+
+        posting = await posting.save();
+        res.redirect("/community/clinics");
+    } catch (error) {
+        res.send(`<script>alert("게시글 작성에 문제가 발생했습니다. 관리자에게 문의하세요."); history.go(-1);</script>`);
+        console.log("*** DB 저장 문제: " + error);
+    }
+})
+
 router.get("/questions", async (req, res) => {
     res.render("community/questions")
 });
@@ -23,5 +43,7 @@ router.get("/questions", async (req, res) => {
 router.get("/questions/:id", async (req, res) => {
     res.render("community/post")
 });
+
+
 
 module.exports = router

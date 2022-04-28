96b935ab65ec1c621d904efc4993f9ea1837387f
kmi0817 <kmi0817@naver.com>
2022-04-28 11:02:17 +0900

    feat: make pagination but sorting problem

--- routes/community.js
+++ routes/community.js
@@ -10,9 +10,17 @@ mongoose.connect("mongodb://localhost/app", {
 });
 
 router.get("/clinics", async (req, res) => {
+    const page = Number(req.query.page || 1); // default page is 1, query는 String이므로 Number로 형변환 필요
+    const perPage = 5
     try {
-        const postings = await Communities.find({ is_deleted: false, community: "clinics" });
-        res.render("community/board", { category: "clinics", postings: postings });
+        const total = await Communities.countDocuments({}); // 총 게시글 개수
+        const postings = await Communities.find({ is_deleted: false, community: "clinics" })
+                                            .sort({ created_at: -1 }) // 최신 글이 먼저 보이도록 함
+                                            .skip(perPage * (page - 1)) // 검색에서 제외할 데이터 개수
+                                            .limit(perPage); // 가져올 포스팅 개수
+        const totalPages = Math.ceil(total / perPage);
+
+        res.render("community/board", { category: "clinics", postings: postings, total: total, totalPages: totalPages });
     } catch (error) {
         console.log("*** " + error);
     }

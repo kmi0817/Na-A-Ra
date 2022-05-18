2b37785a9c527e21bed780fb75bf19adab5273c9
kmi0817 <kmi0817@naver.com>
2022-05-18 15:42:31 +0900

    style: 태그에 대한 설명 추가

--- swagger/swagger.js
+++ swagger/swagger.js
@@ -7,10 +7,35 @@ const options = {
             version: "1.0.0",
             title: "NaARa API",
             description: "NaARa API description...",
+            termsOfService: "http://localhost:3000",
+            contact: {
+                email: "kmi0817@naver.com"
+            }
         },
-        servers: [
+        tags: [
             {
-                url: "http://localhost:3000"
+                name: "관리자",
+                description: "관리자 페이지에서 사용하는 API"
+            },
+            {
+                name: "커뮤니티",
+                description: "커뮤니티 페이지에서 사용하는 API"
+            },
+            {
+                name: "마이페이지",
+                description: "마이페이지 페이지에서 사용하는 API"
+            },
+            {
+                name: "병원 리뷰",
+                description: "병원 리뷰 페이지에서 사용하는 API"
+            },
+            {
+                name: "검색",
+                description: "검색 페이지에서 사용하는 API"
+            },
+            {
+                name: "처리",
+                description: "회원가입, 로그인, 로그아웃, 병원 신고 접수에서 사용하는 API"
             }
         ]
     },

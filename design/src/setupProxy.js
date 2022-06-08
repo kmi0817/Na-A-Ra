48d2ed21632d720c2f3fe5d2bc9f97095d37d31c
kmi0817 <kmi0817@naver.com>
2022-06-08 17:50:26 +0900

    feat: 서버 주소 변경
    로컬호스트에서 연구실 맥 주소로

--- design/src/setupProxy.js
+++ design/src/setupProxy.js
@@ -1,11 +1,11 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
-module.exports = function(app) {
-  app.use(
-    '/api',
-    createProxyMiddleware({
-      target: 'http://localhost:3001',
-      changeOrigin: true,
-    })
-  );
+module.exports = function (app) {
+    app.use(
+        '/api',
+        createProxyMiddleware({
+            target: 'http://220.149.235.76:3001',
+            changeOrigin: true
+        })
+    );
 };
